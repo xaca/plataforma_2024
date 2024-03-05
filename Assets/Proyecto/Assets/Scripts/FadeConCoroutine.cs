a2626467039b7d00a8ca2b282124b87637ba3a41
xaca <and1res@hotmail.com>
2024-03-05 10:07:31 -0500

    Ejemplo de fade, para entender coroutines

---
+++ Assets/Proyecto/Assets/Scripts/FadeConCoroutine.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class FadeConCoroutine : FadeSinCoroutine
+{
+    [SerializeField]
+    private MMF_Player feedback;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void FadeIn()
+    {
+        feedback?.PlayFeedbacks();
+        StartCoroutine(FadeInCoroutine());
+    }
+
+    public override void FadeOut()
+    {
+        StartCoroutine(FadeOutCoroutine());
+    }
+
+    private IEnumerator FadeInCoroutine()
+    {
+        _spriteRenderer = setPlayer();
+        
+        Color c = _spriteRenderer.color;
+        for (float alpha = 0; alpha <= 1; alpha+=.01f)
+        {
+            //Debug.Log("FadeInCoroutine "+alpha);
+            c.a = alpha;
+            _spriteRenderer.color = c;
+            //yield return new WaitForSeconds(0.1f);
+            yield return null;
+        }
+        
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        _spriteRenderer = setPlayer();
+        
+        Color c = _spriteRenderer.color;
+        for (float alpha = 1; alpha > 0; alpha-=.01f)
+        {
+            //Debug.Log("FadeOutCoroutine "+alpha);
+            c.a = alpha;
+            _spriteRenderer.color = c;
+            yield return new WaitForSeconds(0.01f);
+            //yield return null;
+        }
+        
+    }
+}
--- Assets/Proyecto/Assets/Scripts/FadeSinCoroutine.cs
+++ Assets/Proyecto/Assets/Scripts/FadeSinCoroutine.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 
 public class FadeSinCoroutine : MonoBehaviour
 {
-    private GameObject model,sr;
+    
     private GameObject _fadeObject;
-    private SpriteRenderer _spriteRenderer;
+    protected SpriteRenderer _spriteRenderer;
     [Header("Fade")]
     [MMInspectorButton("FadeIn")]
     public bool FadeInButton;
@@ -19,53 +19,49 @@ public class FadeSinCoroutine : MonoBehaviour
     {
         
     }
-    void setPlayer()
+    public SpriteRenderer setPlayer()
     {
+        GameObject model,sr;
         _fadeObject = LevelManager.Instance.Players[0].gameObject;
          model = _fadeObject.transform.Find("Model").gameObject;
         
         if (model == null)
         {
             Debug.LogError("Model not found");
-            return;
         }
         else{
             sr = model.transform.Find("SpriteRenderer").gameObject;
+            if(sr == null)
+            {
+                Debug.LogError("SpriteRenderer not found");
+            }
+            else{
+                SpriteRenderer renderer = sr.GetComponent<SpriteRenderer>();
+                return renderer;
+            }
         }
+        
+        return null;
     }
     public virtual void FadeIn()
     {
-        setPlayer();
-        if(sr == null)
-        {
-            Debug.LogError("SpriteRenderer not found");
-            return;
-        }
-        SpriteRenderer renderer = sr.GetComponent<SpriteRenderer>();
-        Color c = renderer.color;
-        for (int alpha = 0; alpha <= 255; alpha++)
+        _spriteRenderer = setPlayer();
+        
+        Color c = _spriteRenderer.color;
+        for (float alpha = 0; alpha <= 1; alpha+=.01f)
         {
             c.a = alpha;
-            renderer.color = c;
+            _spriteRenderer.color = c;
         }
     }
     public virtual void FadeOut()
     {
-        Debug.Log("Fade");
-        setPlayer();
-        
-        if(sr == null)
-        {
-            Debug.LogError("SpriteRenderer not found");
-            return;
-        }
-
-        SpriteRenderer renderer = sr.GetComponent<SpriteRenderer>();
-        Color c = renderer.color;
-        for (int alpha = 255; alpha >= 0; alpha--)
+        _spriteRenderer = setPlayer();
+        Color c = _spriteRenderer.color;
+        for (float alpha = 1; alpha > 0; alpha-=.01f)
         {
             c.a = alpha;
-            renderer.color = c;
+            _spriteRenderer.color = c;
         }
         
     }
